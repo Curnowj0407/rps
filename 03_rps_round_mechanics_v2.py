c1623f083a15793df0227bce4f105ca338d31212
Curnowj0407 <99618992+Curnowj0407@users.noreply.github.com>
2022-04-11 10:56:51 +1200

    Add files via upload

--- 03_rps_round_mechanics_v2.py
+++ 03_rps_round_mechanics_v2.py
@@ -1,10 +1,10 @@
 # Fuction used to check input is vaild
 
-
 def check_rounds():
 
+
     while True:
-        response = input ("how many rounds: ")
+        response = input ("how many rounds do you want to play ? ")
 
         round_error = "please type either <enter> or an integerthat is more 0"
 
@@ -38,11 +38,7 @@ while end_game == "no":
     print()
     if rounds =="":
         
-        headings = "continue mode: round {}".format(rounds_played)
-        print(headings)
-        choose = input("{} or 'xxx' to end:".format(choose_instruction))
-        if choose == "xxxc":
-            break
+        headings = "continue mode: round {}".format(rounds_played +1)
     else:
         headings = "round {} of {}".format(rounds_played + 1, rounds)
         print(headings)
@@ -50,7 +46,13 @@ while end_game == "no":
         if rounds_played == rounds -1:
             end_game = "yes"
 
-    # rest of loop / ga,e
+    print(headings)
+    choose = input("{} or 'xxx' to end:".format(choose_instruction))
+
+
+    if choose == "xxx":
+        break
+    # rest of loop / game
     print("you choose {}".format(choose))
 
     rounds_played += 1
